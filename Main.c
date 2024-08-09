810fbfd8c48e132d1fb9c62da6fb63d30a773750
Nereya Mantzur <145807285+NereyaMantzur@users.noreply.github.com>
2024-08-09 15:40:44 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,8 +2,36 @@
 
 #include "Library.h"
 
+void PrintMenu();
+
 //program start here
 int main()
 {
-	printf("Dddd");
+Library library;
+initLibrary(*library);
+
+	printf("welcome to the library manager  V1.0");
+
+int choice;
+do{
+ PrintMenu();
+ scanf("%d",&choice);
+ 
+}
+
+void PrintMenu(){
+ printf("1.  add new book/n");
+ printf("2.  remove book/n");
+ printf("3.  add new member/n");
+ printf("4.  remove member/n");
+ printf("5.  add new loan/n");
+ printf("6.  return book loan/n");
+ printf("7.  search a book/n");
+ printf("8.  search a member/n");
+ printf("9.  print all books avaiable/n");
+ printf("10. print all loaned books/n");
+ printf("11. print all members/n");
+ printf("12. print popular books/n");
+ printf("13. Exit program/n");
+ printf("Please enter your choice: ");
 }
